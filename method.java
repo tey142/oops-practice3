ec42c788017e349c0b4113ce020922f07a7b11d0
tey142 <taylor.d.suh@gmail.com>
2026-09-13 15:17:43 -0500

    Add files via upload

---
+++ method.java
@@ -0,0 +1,15 @@
+class vegetable{
+    public void method1(){
+        System.out.println("tomato");
+    }
+    public void method2(){
+        System.out.println("avocado");
+    }
+}
+public class method{
+    public static void main(String[] args){
+    vegetable v=new vegetable();
+    v.method1();
+    v.method2();
+    }
+}
